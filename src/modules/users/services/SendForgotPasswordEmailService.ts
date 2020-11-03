80c79fc4b4d1ff90e5eb54fd53eae060015f3f11
Luann Rodrigo <luann.rodrigos@gmail.com>
2020-11-03 11:15:39 -0300

    add: create tests to send email when password is reseted

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,41 @@
+import { injectable, inject } from 'tsyringe';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import AppError from '@shared/errors/AppError';
+
+interface IRequestDTO {
+  email: string;
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+
+    @inject('UserTokensRepository')
+    private userTokenRepository: IUserTokensRepository,
+  ) {}
+
+  public async execute({ email }: IRequestDTO): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exists.');
+    }
+
+    await this.userTokenRepository.generate(user.id);
+
+    this.mailProvider.sendMail(
+      email,
+      'Pedido de recuperação de senha recebido',
+    );
+  }
+}
+
+export default SendForgotPasswordEmailService;
